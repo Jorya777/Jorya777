4b3eb460bf4c782dfa8329a5df238ec168fae625
Jorya777 <550910750@qq.com>
2025-10-25 22:22:00 -0400

    Update title to 'UN Sustainable Development Cooperation Framework Evaluation Dashboard'

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ st.markdown("""
              style="height:130px; margin-right:40px;">
         <div>
             <h1 style="color:white; font-size:40px; font-weight:700; line-height:1.2; margin:0;">
-                UNSDCF Evaluation Dashboard (2021–2024)
+                UN Sustainable Development Cooperation Framework Evaluation Dashboard (2021–2024)
             </h1>
         </div>
     </div>
